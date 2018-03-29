b1827fe46986a0e3b049b51d002cd3c36b92acd7
Akhtar Shamim <ashamim@wwex.com>
2018-03-28 23:29:27 -0500

    Initial setup

--- codeville/src/codeville/Codeville.java
+++ codeville/src/codeville/Codeville.java
@@ -19,7 +19,7 @@ public class Codeville extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("ui/welcome/welcome.fxml"));
         
         Scene scene = new Scene(root);
         
